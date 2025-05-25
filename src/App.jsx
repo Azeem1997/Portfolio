1ef9558d14515dec6bb036fa4928094c4b3bad63
Azeem1997 <azeemrinvaz97@gmail.com>
2025-05-25 23:05:12 +0530

    motion changes

--- src/App.jsx
+++ src/App.jsx
@@ -9,18 +9,43 @@ import Navbar from './components/Navbar'
 import { Route, Routes } from 'react-router-dom'
 import Projects from './pages/Projects';
 import Qualification from './pages/Qualification';
+import { motion, MotionConfig } from 'framer-motion';
 
 function App() {
 
+  const containerVariants = {
+  hidden: { opacity: 0 },
+  visible: {
+    opacity: 1,
+    transition: {
+      staggerChildren: 0.4, // delay between children
+    },
+  },
+};
+
+const itemVariants = {
+  hidden: { opacity: 0, y: 50 }, // 50px down
+  visible: {
+    opacity: 1,
+    y: 0,
+    transition: { duration: 0.7, ease: "easeOut" },
+  },
+};
+
   return (
-      <div className="">
-      <Navbar/>
+     <motion.div
+      variants={containerVariants}
+      initial="hidden"
+      animate="visible"
+      className=""
+    >
+      <Navbar />
       <Home />
-      <About />
-      <Projects />
-      <Qualification />
-      <ToastContainer/>
-      </div>
+      <motion.div variants={itemVariants}><About /></motion.div>
+      <motion.div variants={itemVariants}><Projects /></motion.div>
+      <motion.div variants={itemVariants}><Qualification /></motion.div>
+      <motion.div variants={itemVariants}><ToastContainer /></motion.div>
+    </motion.div>
   )
 }
 
