15d951ba847ebb96dc6565f21b38d160112098b3
Azeem1997 <azeemrinvaz97@gmail.com>
2025-05-25 17:56:48 +0530

    New Puch

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,15 +1,16 @@
 // src/pages/Home.jsx
 import { motion } from 'framer-motion';
-
+import img from '../../public/Azeem_Full.jpg'
 export default function Home() {
   return (
-<>
+    <div className='bg-black text-white text-center py-16'>
+      <img src={img} alt='Azeem' className='mx-auto mb-8 w-48 h-48 rounded-full object-cover transform transition-transform duration-300 hover:scale-105'/>
       <h1 className="text-3xl font-bold">
-        Hi, I'm Azeem
+        Hi, I'm <span className='text-transparent bg-clip-text bg-gradient-to-r from-green-400 to-blue-500'>Azeem</span>
       </h1>
-      <p className="text-3xl">
-        I'm a passionate UI/UX Developer with a love for clean design, smooth UX, and modern web tech.
+      <p className="mt-4 text-lg text-gray-300">
+        A Software Engineer specialized in building modern and responsive web application
       </p>
-      </>
+    </div>
   );
 }
